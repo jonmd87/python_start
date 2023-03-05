36a995b6d56a1d2849f35087957c926498df3b52
jonmd87 <jonmd87@yandex.com>
2023-03-05 13:33:05 +0100

    task18 done

--- python_seminar/seminar3/Homework/task18.py
+++ python_seminar/seminar3/Homework/task18.py
@@ -8,3 +8,24 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+
+import random
+
+quantity = int(input('enter number of positions: '))
+list = []
+
+for i in range(quantity):
+    list.append(random.randint(0, quantity))
+
+print(list)
+number = int(input('now enter number that i need to search: '))
+flag = True
+index = 0
+while flag:
+    if list.count(number - index) > 0:
+        flag = False
+        print(number - index)
+    if list.count(number + index) > 0:
+        flag = False
+        print(number + index)
+    index += 1
