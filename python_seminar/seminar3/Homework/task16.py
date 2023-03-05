58ddb064f57960ed3d95b8054a8896dbb0eb3225
jonmd87 <jonmd87@yandex.com>
2023-03-05 12:53:37 +0100

    task 16 done

--- python_seminar/seminar3/Homework/task16.py
+++ python_seminar/seminar3/Homework/task16.py
@@ -8,3 +8,18 @@
 #     1 2 3 4 5
 #     3
     # -> 1
+
+import random
+
+list = []
+quantity = int(input('Enter number of positions: '))
+for i in range(quantity):
+    list.append(random.randint(0, 10))
+
+number = int(input('Now enter number(from 1 to 10): '))
+counter = 0
+for i in list:
+    if i == number:
+        counter += 1
+print(list)
+print(f'{number} --> {counter}')
