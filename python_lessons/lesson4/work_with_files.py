d90347b0aa952d660607ef9b55d4eced2fcc8f7d
jonmd87 <jonmd87@yandex.com>
2023-03-16 05:51:09 +0100

    lessons4

---
+++ python_lessons/lesson4/work_with_files.py
@@ -0,0 +1,6 @@
+# mode():
+# [ a ] - add data, if file does't exist file will be crated
+# [ r ] - read data, if file doesn't exist program will show ERROR
+# [ w ] - write data, if file does't exist file will be crated
+# [ r+ ] - read/write data from file, if file does't exist file will be crated
+# [ w+ ] - read/add data from file, if file does't exist file will be crated
